b93b039ad06f48ad58e35dd8f94f7e629fe73103
Matan Avital <matavital13@gmail.com>
2023-01-07 16:56:58 +0200

    adding files

---
+++ Terraform/main.tf
@@ -0,0 +1,169 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  azs = data.aws_availability_zones.available.names
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "main" {
+  count = 3
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.${count.index + 1}.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = local.azs["${count.index}"]
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table_association" "main" {
+  count = 3
+  subnet_id      = aws_subnet.main[count.index].id
+  route_table_id = aws_route_table.main.id
+}
+
+
+resource "aws_route" "main" {
+  route_table_id         = aws_route_table.main.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+
+resource "aws_security_group" "allow_ssh_http" {
+  name = "allow_ssh_http"
+  description = "Allow SSH and HTTP access"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ansible_host" {
+  count = 2
+  ami           = "ami-079bae937cca87cfd"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main["${count.index}"+1].id
+  key_name      = "ansible_key"
+  security_groups = [aws_security_group.allow_ssh_http.id]
+
+  tags = {
+    Name = "ansible-host_${count.index+1}"
+  }
+}
+
+
+
+resource "aws_instance" "ansible_master" {
+  ami           = "ami-079bae937cca87cfd"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main[1].id
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+  key_name      = "ansible_key"
+  user_data = base64encode(<<EOF
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install -y python3
+curl -kL https://bootstrap.pypa.io/get-pip.py | python3
+pip install ansible
+
+EOF
+)
+  tags = {
+    Name = "ansible_master"
+    Environment = "dev"
+  }
+}
+
+
+output "instance_1_ip" {
+  value = aws_instance.ansible_host[0].public_ip
+}
+
+output "instance_2_ip" {
+  value = aws_instance.ansible_host[1].public_ip
+}
+
+resource "local_file" "ec2_ips" {
+  /* count = 2 */
+  content  = join("\n", [aws_instance.ansible_host[0].public_ip, aws_instance.ansible_host[1].public_ip])
+  filename = "ec2_ips.txt"
+}
+
+
+
+
+resource "aws_iam_role" "ec2_ami_access" {
+  name = "test_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_policy" "ami_access" {
+  name = "ami_access_policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "ec2:DescribeImages",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "ami_access_attachment" {
+  name       = "ami_access_attachment"
+  policy_arn = aws_iam_policy.ami_access.arn
+  roles      = [aws_iam_role.ec2_ami_access.name]
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_prefix" {
+  type = string
+  default = "16"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = ""
+  default     = "10.0.0.0/16"
+
+}
